9ff5ba14e3d0d8b16d7eee5feabc917e581d4edc
Rajeshgottimukkula <rajesh.gottimukkula1@gmail.com>
2024-09-14 15:13:51 +0530

    Add entities, repos , controller

---
+++ src/main/java/com/example/accounts/entity/Customer.java
@@ -0,0 +1,29 @@
+package com.example.accounts.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Customer extends BaseEntity{
+
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="customer_id")
+    private Long customerId;
+
+    private String name;
+
+    private String email;
+
+    @Column(name="mobile_number")
+    private String mobileNumber;
+
+}
